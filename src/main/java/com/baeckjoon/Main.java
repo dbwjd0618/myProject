608140a909286f6e81851ecd4239f4b9e4b7d0d3
yjkim <yjkim@crizen.com>
2023-06-23 09:55:50 +0900

    백준 - 10811.바구니 뒤집기 / 진행중

--- src/main/java/com/baeckjoon/Main.java
+++ src/main/java/com/baeckjoon/Main.java
@@ -1,24 +1,51 @@
 package com.baeckjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		int[] students = new int[31];
-		for(int j = 0; j < 28; j ++) {
-			int submited = sc.nextInt();
-			students[submited] = 1;
+		/**
+		 * 10811.i~j번까지의 바구니를 역순으로 정렬
+		 * 5 4 : N(바구니), M(횟수)
+		 * 1 2 : 2 1 3 4 5
+		 * 3 4 : 2 1 4 3 5
+		 * 1 4 : 3 4 1 2 5
+		 * 2 2 : 3 4 1 2 5
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		String[] terms = sc.nextLine().split(" ");
+		int N = Integer.parseInt(terms[0]);
+		int M = Integer.parseInt(terms[1]);
+		
+		int[] backets = new int[N];
+		int[] tmp = new int[N];
+		
+		// 최초 데이터 세팅
+		for(int i = 0; i < N; i ++) {
+			backets[i] = i+1;
 		}
 		
-		for(int i = 1; i < students.length; i ++) {
-			if(students[i] != 1) {
-				System.out.println(i);
+		tmp = backets;
+		for(int j = 0; j < M; j ++) {
+			String[] section = sc.nextLine().split(" ");
+			int start = Integer.parseInt(section[0])-1;
+			int end = Integer.parseInt(section[1])-1;
+			
+			if(end-start > 1) {
+				for(int k = 0; k <= end-start; k ++) {
+					backets[k] = tmp[end-k];
+				}
+			} else {
+				backets[start] = tmp[end];
+				backets[end] = tmp[start];
 			}
 		}
 		
+		System.out.println(Arrays.toString(backets));
 	}
 }
