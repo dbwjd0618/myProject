0460d205ffd0b560348b57c1564c64d089e3e1ae
KSH <KSH@DESKTOP-KCJUCP6>
2023-06-25 19:28:35 +0900

    백준 - 10809. 알파벳 찾기

--- src/main/java/com/baeckjoon/Main.java
+++ src/main/java/com/baeckjoon/Main.java
@@ -1,20 +1,40 @@
 package com.baeckjoon;
 
 import java.util.Scanner;
-
 public class Main {
 	
 	public static void main(String[] args) {
 	
+		/**
+		 * baekjoon
+		 * a b  c  d e  f  g  h  i j k  l  m n o  p  q  r  s  t  u  v  w  x  y  z
+		 * 1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+		 */
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		String sentence = sc.next();
+		String word = sc.nextLine();
 		
-		int sum = 0;
-		for(int i = 0; i < N; i++) {
-			sum += sentence.charAt(i)-48;
+		// 26개
+		String[] arr = new String[26];
+		char c = ' ';
+		int j = 0;
+		for(char i = 97; i < 123; i ++) {
+			c = i;
+			arr[j] = String.valueOf(c);
+			j ++;
 		}
 		
-		System.out.println(sum);
+		String result = "";
+		for(int m = 0; m < arr.length; m ++) {
+			String alpha = arr[m];
+			
+			if(word.contains(alpha)) {
+				arr[m] = String.valueOf(word.indexOf(alpha));
+			} else {
+				arr[m] = "-1";
+			}
+			result += arr[m] + " ";
+		}
+		System.out.println(result.trim());
+		sc.close();
 	}
 }
--- src/main/java/com/baeckjoon/problems/StringProblem.java
+++ src/main/java/com/baeckjoon/problems/StringProblem.java
@@ -6,7 +6,36 @@ public class StringProblem {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		stringTonumbersSum(sc);		// 11720.숫자의 합
+		stringTonumbersSum(sc);	// 11720. 숫자의 합
+        searchAlphabet(sc);		// 10809. 알파벳 찾기
+	}
+
+	private static void searchAlphabet(Scanner sc) {
+		String word = sc.nextLine();
+		
+		// 26개
+		String[] arr = new String[26];
+		char c = ' ';
+		int j = 0;
+		for(char i = 97; i < 123; i ++) {
+			c = i;
+			arr[j] = String.valueOf(c);
+			j ++;
+		}
+		
+		String result = "";
+		for(int m = 0; m < arr.length; m ++) {
+			String alpha = arr[m];
+			
+			if(word.contains(alpha)) {
+				arr[m] = String.valueOf(word.indexOf(alpha));
+			} else {
+				arr[m] = "-1";
+			}
+			result += arr[m] + " ";
+		}
+		System.out.println(result.trim());
+		sc.close();
 	}
 
 	private static void stringTonumbersSum(Scanner sc) {
